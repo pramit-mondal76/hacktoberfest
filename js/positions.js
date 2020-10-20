cb656ab4a488056d788294c7d6bcf18851e9a33e
illusion <dhruvtiwari110058@gmail.com>
2020-10-20 13:42:13 +0530

    added dark mode to branch

--- js/positions.js
+++ js/positions.js
@@ -1,10 +1,11 @@
 // This file include name and place of Contributors.
 // Please write your name and coordinates in the give format
-// { name:'<github id>', lat: ?, lng: ? }, 
+// { name:'<github id>', lat: ?, lng: ? },
 
 var positions = [
-		{ name:'tchesa', lat: -19.915535, lng: -43.967633 }, 
-		{ name:'matteomessmer', lat: 41.2925, lng: 12.5736 },
-    { name:'harsh863', lat: 28.667856, lng: 77.449791 },
-    { name:'onneri', lat: 22.1140758, lng: -100.7974705 },
-]
+  { name: "tchesa", lat: -19.915535, lng: -43.967633 },
+  { name: "matteomessmer", lat: 41.2925, lng: 12.5736 },
+  { name: "harsh863", lat: 28.667856, lng: 77.449791 },
+  { name: "onneri", lat: 22.1140758, lng: -100.7974705 },
+  { name: "illusion47586", lat: 28.6227117, lng: -77.0909803 },
+];
--- js/themeToggle.js
+++ js/themeToggle.js
@@ -1,16 +1,18 @@
-const switchBtn = document.querySelector('.switch_1');
+const switchBtn = document.querySelector(".switch_1");
 
-switchBtn.addEventListener('click', (event) => {
-    if (event.target.checked) {
-        document.body.className = 'dark-mode';
-        localStorage.setItem('mode', 'dark');
-    } else {
-        localStorage.removeItem('mode');
-        document.body.classList = [];
-    }
+switchBtn.addEventListener("click", (event) => {
+  if (event.target.checked) {
+    document.body.className = "dark-mode";
+    localStorage.setItem("mode", "dark");
+    document.querySelector("#osmMap canvas").style.filter = "invert(100%)";
+  } else {
+    localStorage.removeItem("mode");
+    document.body.classList = [];
+    document.querySelector("#osmMap canvas").style.filter = "";
+  }
 });
 
-if (localStorage.getItem('mode') === 'dark') {
-    switchBtn.checked = true;
-    document.body.className = 'dark-mode';
+if (localStorage.getItem("mode") === "dark") {
+  switchBtn.checked = true;
+  document.body.className = "dark-mode";
 }
